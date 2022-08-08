34882195625b8e8b710bf7b972d128b57c535462
PowersYang <577790911@qq.com>
2022-08-08 22:21:28 +0800

    update java

--- Java/src/main/java/com/sqlboy/annotation/AnnotationTest.java
+++ Java/src/main/java/com/sqlboy/annotation/AnnotationTest.java
@@ -1,20 +1,68 @@
 package com.sqlboy.annotation;
 
-import jdk.nashorn.internal.ir.annotations.Reference;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
-@MyAnnotation(reportedBy = "sqlboy")
 public class AnnotationTest {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        MyAnnotationHandler myAnnotationHandler = new MyAnnotationHandler();
+        myAnnotationHandler.handler();
     }
 }
 
-@interface MyAnnotation {
-    enum Status{UNCONFIRMED, CONFIRMED, FIXED, NOTBUG};
-    boolean showStopper() default false;
-    String assignedTo() default "[none]";
-    Class<?> testCase() default Void.class;
-    Status status() default Status.UNCONFIRMED;
-    Reference fer() default @Reference;
-    String[] reportedBy();
+class MyAnnotationHandler {
+    public void handler() throws Exception {
+        Class<MyService> clazz = MyService.class;
+        MyService myService = clazz.newInstance();
+        Method[] methods = clazz.getDeclaredMethods();
+
+        List<Method> beforeMethodList = new ArrayList<>();
+        List<Method> testMethodList = new ArrayList<>();
+        List<Method> afterMethodList = new ArrayList<>();
+
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyBefore.class)) {
+                beforeMethodList.add(method);
+            }
+
+            if (method.isAnnotationPresent(MyTest.class)) {
+                testMethodList.add(method);
+            }
+
+            if (method.isAnnotationPresent(MyAfter.class)) {
+                afterMethodList.add(method);
+            }
+        }
+
+        for (Method testMethod : testMethodList) {
+            for (Method beforeMethod : beforeMethodList) {
+                beforeMethod.invoke(myService);
+            }
+
+            testMethod.invoke(myService);
+
+            for (Method afterMethod : afterMethodList) {
+                afterMethod.invoke(myService);
+            }
+        }
+    }
+}
+
+
+class MyService {
+    @MyBefore
+    public void before() {
+        System.out.println("获取数据库连接");
+    }
+
+    @MyTest
+    public void test() {
+        System.out.println("处理数据");
+    }
+
+    @MyAfter
+    public void after() {
+        System.out.println("释放数据库连接资源");
+    }
 }
