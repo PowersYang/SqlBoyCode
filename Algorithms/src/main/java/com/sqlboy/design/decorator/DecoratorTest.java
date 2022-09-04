1eb06c7c63e137902f8cf157e2a8bcedf8bc8a96
PowersYang <577790911@qq.com>
2022-09-04 21:49:35 +0800

    update design pattern

--- Algorithms/src/main/java/com/sqlboy/design/decorator/DecoratorTest.java
+++ Algorithms/src/main/java/com/sqlboy/design/decorator/DecoratorTest.java
@@ -1,10 +1,11 @@
 package com.sqlboy.design.decorator;
 
-import lombok.ToString;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DecoratorTest {
     public static void main(String[] args) {
-        Automobile automobile = new BaseAutomobile("2.0T", "双离合", "承载式");
+        Automobile automobile = new BaseAutomobile();
         automobile.makeAutomobile();
 
         automobile = new AccDecorator(automobile);
@@ -16,40 +17,33 @@ public class DecoratorTest {
 }
 
 interface Automobile {
+    // 配置清单
+    List<String> confList = new ArrayList<>();
+
     void makeAutomobile();
 }
 
 /**
  * 基础汽车类
  */
-@ToString(callSuper = true)
 class BaseAutomobile implements Automobile {
 
-    private String engine;
-    private String gearbox;
-    private String chassisSuspension;
 
-    public BaseAutomobile(String engine, String gearbox, String chassisSuspension) {
-        this.engine = engine;
-        this.gearbox = gearbox;
-        this.chassisSuspension = chassisSuspension;
+    public BaseAutomobile() {
+        confList.add("2.0T Engine");
+        confList.add("双离合变速箱");
+        confList.add("承载式车身");
     }
 
     @Override
     public void makeAutomobile() {
-        System.out.println(this);
-    }
-
-    @Override
-    public String toString() {
-        return "BaseAutomobile{" + "engine='" + engine + '\'' + ", gearbox='" + gearbox + '\'' + ", chassisSuspension='" + chassisSuspension + '\'' + '}';
+        System.out.println(confList);
     }
 }
 
-@ToString
 class AutomobileDecorator implements Automobile {
 
-    private Automobile automobile;
+    public Automobile automobile;
 
     public AutomobileDecorator(Automobile automobile) {
         this.automobile = automobile;
@@ -64,10 +58,7 @@ class AutomobileDecorator implements Automobile {
 /**
  * ACC功能装饰器
  */
-@ToString(callSuper = true)
 class AccDecorator extends AutomobileDecorator {
-
-    private String acc;
 
     public AccDecorator(Automobile automobile) {
         super(automobile);
@@ -76,17 +67,18 @@ class AccDecorator extends AutomobileDecorator {
     @Override
     public void makeAutomobile() {
         this.setAcc();
-        System.out.println(this);
+        System.out.println(confList);
     }
 
     private void setAcc() {
-        this.acc = "acc";
+        confList.add("acc");
     }
 }
 
+/**
+ * 倒车雷达装饰类
+ */
 class ReversingRadarDecorator extends AutomobileDecorator {
-
-    private String reversingRadar;
 
     public ReversingRadarDecorator(Automobile automobile) {
         super(automobile);
@@ -95,10 +87,10 @@ class ReversingRadarDecorator extends AutomobileDecorator {
     @Override
     public void makeAutomobile() {
         this.setReversingRadar();
-        System.out.println(this);
+        System.out.println(confList);
     }
 
     public void setReversingRadar() {
-        this.reversingRadar = "reversingRadar";
+        confList.add("reversingRadar");
     }
 }
