ab4d4e8ec538ff78c75f09a6cf2063bb0fd2970e
PowersYang <577790911@qq.com>
2023-03-07 16:54:25 +0800

    update

--- Algorithms/src/main/java/com/sqlboy/sort/BubblingSort.java
+++ Algorithms/src/main/java/com/sqlboy/sort/BubblingSort.java
@@ -10,23 +10,13 @@ public class BubblingSort {
     }
 
     public static void sort(int[] arrays){
-        int temp = 0;
-        boolean flag = false;
         for (int i = 0; i < arrays.length - 1; i++) {
             for (int j = 0; j < arrays.length - 1 - i; j++) {
                 if (arrays[j] > arrays[j + 1]) {
-                    flag = true;
-                    temp = arrays[j];
+                    int temp = arrays[j];
                     arrays[j] = arrays[ j + 1];
                     arrays[j + 1] = temp;
                 }
-            }
-
-            if (!flag) {
-                // 在排序过程中，没有发生一次交换
-                break;
-            } else {
-                flag = false;
             }
         }
 
